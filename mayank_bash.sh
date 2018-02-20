0364989f7cb31b74a678479e24b89ecdc0ca7149
Mayank Sharma <masharma@factset.com>
2018-02-19 19:38:49 -0500

    added git branch to ps1 line

--- mayank_bash.sh
+++ mayank_bash.sh
@@ -13,6 +13,7 @@ alias lls="ls --color='always' -alSrh --group-directories-first"
 alias ls="ls --color='always'"
 alias llgrep='ll | grep'
 alias vi='vim'
+alias path='readlink -e'
 
 # TMUX aliases
 alias t='tmux'
@@ -23,8 +24,12 @@ alias tkill='t kill-session -t'
 alias w40='watch -n 1 tail -n 40'
 alias w30='watch -n 1 tail -n 30'
 
+parse_git_branch() {
+    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1) /'
+}
+
 # Dark Background
-export PS1="\[\e[00;37m\][\[\e[0m\]\[\e[00;31m\]\u\[\e[0m\]\[\e[00;37m\]@\[\e[0m\]\[\e[00;34m\]\h\[\e[0m\]\[\e[00;37m\] \[\e[0m\]\[\e[00;36m\]\t\[\e[0m\]\[\e[00;37m\] \[\e[0m\]\[\e[00;32m\]\w\[\e[0m\]\[\e[00;37m\]]\$ \[\e[0m\]"
+export PS1="\[\e[00;37m\][\[\e[0m\]\[\e[00;31m\]\u\[\e[0m\]\[\e[00;37m\]@\[\e[0m\]\[\e[00;34m\]\h\[\e[0m\]\[\e[00;37m\] \[\e[0m\]\[\e[00;36m\]\t\[\e[0m\]\[\e[00;37m\] \[\e[0m\]\[\e[00;32m\]\w\[\e[0m\]\[\e[00;37m\]]\$(parse_git_branch)\$ \[\e[0m\]"
 
 # Light Background
 #export PS1="[\[$(tput sgr0)\]\[\033[38;5;196m\]\u\[$(tput sgr0)\]\[\033[38;5;0m\]@\[$(tput sgr0)\]\[\033[38;5;4m\]\h\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]\[\033[38;5;6m\]\T\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]\[\033[38;5;2m\]\w\[$(tput sgr0)\]\[\033[38;5;0m\]]\\$ \[$(tput sgr0)\]"
