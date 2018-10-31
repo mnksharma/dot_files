ede843cfa7b22e2360d25c8e3fe82d8e614496fa
Mayank Sharma <masharma@factset.com>
2018-10-31 13:06:42 -0400

    created git push/pull origin <branch> functions

--- mayank_bash.sh
+++ mayank_bash.sh
@@ -25,11 +25,31 @@ alias w40='watch -n 1 tail -n 40'
 alias w30='watch -n 1 tail -n 30'
 
 parse_git_branch() {
-    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1) /'
+    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
+}
+
+pull(){
+    current_branch=`parse_git_branch`
+    if [[ -z $current_branch ]]; then
+        echo "This is not a git repo"
+        return
+    fi
+    echo "Pulling origin/$current_branch"
+    git pull origin $current_branch
+}
+
+push(){
+    current_branch=`parse_git_branch`
+    if [[ -z $current_branch ]]; then
+        echo "This is not a git repo"
+        return
+    fi
+    echo "Pushing origin/$current_branch"
+    git pull origin $current_branch
 }
 
 # Dark Background
-export PS1="\[\e[00;37m\][\[\e[0m\]\[\e[00;31m\]\u\[\e[0m\]\[\e[00;37m\]@\[\e[0m\]\[\e[00;34m\]\h\[\e[0m\]\[\e[00;37m\] \[\e[0m\]\[\e[00;36m\]\t\[\e[0m\]\[\e[00;37m\] \[\e[0m\]\[\e[00;32m\]\w\[\e[0m\]\[\e[00;37m\]]\$(parse_git_branch)\$ \[\e[0m\]"
+export PS1="\[\e[00;37m\][\[\e[0m\]\[\e[00;31m\]\u\[\e[0m\]\[\e[00;37m\]@\[\e[0m\]\[\e[00;34m\]\h\[\e[0m\]\[\e[00;37m\] \[\e[0m\]\[\e[00;36m\]\t\[\e[0m\]\[\e[00;37m\] \[\e[0m\]\[\e[00;32m\]\w\[\e[0m\]\[\e[00;37m\]] (`parse_git_branch`) \$ \[\e[0m\]"
 
 # Light Background
 #export PS1="[\[$(tput sgr0)\]\[\033[38;5;196m\]\u\[$(tput sgr0)\]\[\033[38;5;0m\]@\[$(tput sgr0)\]\[\033[38;5;4m\]\h\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]\[\033[38;5;6m\]\T\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]\[\033[38;5;2m\]\w\[$(tput sgr0)\]\[\033[38;5;0m\]]\\$ \[$(tput sgr0)\]"
